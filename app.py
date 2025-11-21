37e2d34c61ce84d37a776a559e768bbdc62928b4
richard250331-byte <richard250331@gmail.com>
2025-11-21 03:15:59 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask,request,render_template
 import joblib
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ def index():
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    q=request.form.get("q")
+    q = request.form.get("q")
     print(q)
     return(render_template("main.html"))
 
@@ -24,6 +24,6 @@ def dbs_prediction():
     model = joblib.load("dbs.pkl")
     r = model.predict([[q]])
     return(render_template("dbs_prediction.html",r=r[0][0]))
-    
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run()
